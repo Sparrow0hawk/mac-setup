c6b6da56c06e1e84a98dac165c3fc3491caec390
Sparrow0hawk <alexjcoleman@gmail.com>
2023-10-04 14:41:01 +0100

    add scripts for setting up brew and tig

---
+++ brew.sh
@@ -0,0 +1,6 @@
+#!/bin/bash env
+
+# install brew 
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
---
+++ tig.sh
@@ -0,0 +1,5 @@
+#!/bin/bash env
+
+brew install tig
+
+
