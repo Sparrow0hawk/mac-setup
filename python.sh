2f54f5019a722ce393445ec9179b2e8c0e38fd29
Sparrow0hawk <alexjcoleman@gmail.com>
2023-11-30 20:49:10 +0000

    Update python for 3.12

--- python.sh
+++ python.sh
@@ -5,6 +5,8 @@
 
 set -e
 
+PY_VER=3.12.0
+
 if [ $# -eq 0 ]
 	then
 		echo "No arguments provided"
@@ -12,13 +14,13 @@ fi
 case "$1" in
 	install)
 		cd ~/Downloads
-        curl -LO https://www.python.org/ftp/python/3.11.5/python-3.11.5-macos11.pkg
-		sudo installer -pkg python-3.11.5-macos11.pkg -target /
-        rm python-3.11.5-macos11.pkg
+        curl -LO https://www.python.org/ftp/python/$PY_VER/python-$PY_VER-macos11.pkg
+		sudo installer -pkg python-$PY_VER-macos11.pkg -target /
+        rm python-$PY_VER-macos11.pkg
         cd -
         ;;
 	uninstall)
-		sudo rm -rf /Applications/Python\ 3.11/
+		sudo rm -rf /Applications/Python\ 3.12/
         sudo rm -rf /Library/Frameworks/Python.framework/
 		;;
 esac
