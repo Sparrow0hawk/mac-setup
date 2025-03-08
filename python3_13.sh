7f50f77f629d965ef569257f8280c6edb533bf2c
Alex Coleman <hello@alexjcoleman.me>
2025-03-08 19:23:37 +0000

    Install python 3.13

---
+++ python3_13.sh
@@ -0,0 +1,28 @@
+#!/bin/bash env
+
+# script for installing python 3.13
+# 
+
+set -e
+
+PY_VER=3.13.0
+
+if [ $# -eq 0 ]
+	then
+		echo "No arguments provided"
+fi
+case "$1" in
+	install)
+		cd ~/Downloads
+        curl -LO https://www.python.org/ftp/python/$PY_VER/python-$PY_VER-macos11.pkg
+		sudo installer -pkg python-$PY_VER-macos11.pkg -target /
+        rm python-$PY_VER-macos11.pkg
+        cd -
+        ;;
+	uninstall)
+		sudo rm -rf /Applications/Python\ 3.13/
+        sudo rm -rf /Library/Frameworks/Python.framework/
+		;;
+esac
+
+
